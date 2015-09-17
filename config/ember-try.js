acd9f8e68e0c656282b20e36d319fb966f3ea078
Michael de Hoog <michael.dehoog@gmail.com>
2015-09-17 16:15:18 +1000

    Added 1.13 scenario, and added ember-data to scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,30 +5,47 @@ module.exports = {
       dependencies: { }
     },
     {
-      name: 'ember-release',
+      name: 'ember-1.13',
       dependencies: {
-        'ember': 'components/ember#release'
+        'ember': '~1.13.0',
+        'ember-data': '~1.13.0'
       },
       resolutions: {
-        'ember': 'release'
+        'ember': '~1.13.0',
+        'ember-data': '~1.13.0'
+      }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#release'
+      },
+      resolutions: {
+        'ember': 'release',
+        'ember-data': 'release'
       }
     },
     {
       name: 'ember-beta',
       dependencies: {
-        'ember': 'components/ember#beta'
+        'ember': 'components/ember#beta',
+        'ember-data': 'components/ember-data#beta'
       },
       resolutions: {
-        'ember': 'beta'
+        'ember': 'beta',
+        'ember-data': 'beta'
       }
     },
     {
       name: 'ember-canary',
       dependencies: {
-        'ember': 'components/ember#canary'
+        'ember': 'components/ember#canary',
+        'ember-data': 'components/ember-data#canary'
       },
       resolutions: {
-        'ember': 'canary'
+        'ember': 'canary',
+        'ember-data': 'canary'
       }
     }
   ]
