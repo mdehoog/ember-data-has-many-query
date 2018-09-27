4d0c293add6146a2d499d897e5ded87d309e20ad
Anthony Mangano <amangano@largus.fr>
2018-09-27 19:27:28 +0200

    [WIP] Fix integration tests

--- tests/integration/query-has-many-test.js
+++ tests/integration/query-has-many-test.js
@@ -54,13 +54,17 @@ test('Querying has-many relationship generates correct URL parameters', function
 
   var done = assert.async();
   run(function () {
-    env.store.findRecord('post', 5).then(assert.wait(function (post) {
+    let post;
+    env.store.findRecord('post', 5).then(function (postRecord) {
+      post = postRecord;
       requiredUrl = '/posts/5/comments?page=1';
-      return post.query('comments', {page: 1}).then(assert.wait(function () {
-        requiredUrl = '/posts/5/comments?page=2';
-        return post.query('comments', {page: 2});
-      }));
-    })).then(function () {
+      return post.query('comments', {page: 1})
+    })
+    .then(function () {
+      requiredUrl = '/posts/5/comments?page=2';
+      return post.query('comments', {page: 2});
+    })
+    .then(function () {
       assert.equal(ajaxCalledCount, 2, 'Adapter ajax function was called to query has-many relationship');
       done();
     });
@@ -81,20 +85,28 @@ test('Querying has-many relationship multiple times doesn\'t clear belongs-to-st
     var page = queryString.match(/^.*(\d+)$/)[1];
     return resolve({comments: [{id: page * 2}, {id: page * 2 + 1}]});
   };
-
+  var done = assert.async();
   run(function () {
-    env.store.findRecord('post', 5).then(assert.wait(function (post) {
-      return post.query('comments', {page: 1}).then(assert.wait(function (comments1) {
-        var comments1Copy = comments1.slice(0);
-        return post.query('comments', {page: 2}).then(assert.wait(function (comments2) {
-          comments1Copy.forEach(function (comment) {
-            assert.equal(comment.get('post.id'), 5, 'belongs-to association sticky after multiple has-many queries');
-          });
-          comments2.forEach(function (comment) {
-            assert.equal(comment.get('post.id'), 5, 'belongs-to association correct');
-          });
-        }));
-      }));
-    }));
+    let post;
+    let comments1;
+    let comments2;
+    env.store.findRecord('post', 5).then(function (postRecord) {
+      post = postRecord;
+      return post.query('comments', {page: 1})
+    })
+    .then(function (commentsRecords1) {
+      comments1 = commentsRecords1;
+      return post.query('comments', {page: 2})
+    })
+    .then(function (commentsRecords2) {
+      comments2 = commentsRecords2;
+      comments1.forEach(function (comment) {
+        assert.equal(comment.get('post.id'), 5, 'belongs-to association sticky after multiple has-many queries ');
+      });
+      comments2.forEach(function (comment) {
+        assert.equal(comment.get('post.id'), 5, 'belongs-to association correct');
+      });
+      done();
+    });
   });
 });
