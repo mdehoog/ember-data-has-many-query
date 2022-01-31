386bfd9f2da4ef8d03b898d66d8aeea38dc3152e
Charles Fries <charliefries@icloud.com>
2022-01-31 15:58:02 -0800

    Cleanup

--- tests/dummy/app/models/post.js
+++ tests/dummy/app/models/post.js
@@ -1,7 +1,7 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 import QueryableModelMixin from 'ember-data-has-many-query/mixins/queryable-model';
 
-export default class CommentModel extends Model.extend(QueryableModelMixin) {
+export default class PostModel extends Model.extend(QueryableModelMixin) {
   @attr('string') text;
   @hasMany('comment', { async: true }) comments;
 }
